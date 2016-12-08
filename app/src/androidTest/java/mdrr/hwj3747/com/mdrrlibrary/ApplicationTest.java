dbca8c3c26c55e576e7827686d417dec47116c47
hwj3747 <173972856@qq.com>
2016-12-08 15:04:32 +0800

    Initial commit

---
+++ app/src/androidTest/java/mdrr/hwj3747/com/mdrrlibrary/ApplicationTest.java
@@ -0,0 +1,13 @@
+package mdrr.hwj3747.com.mdrrlibrary;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/test/java/mdrr/hwj3747/com/mdrrlibrary/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package mdrr.hwj3747.com.mdrrlibrary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
